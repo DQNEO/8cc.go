7037a8bf24d073ace37f8860265032eca6b52fa4
DQNEO <dqneoo@gmail.com>
2018-04-30 17:05:57 +0900

    avoid using printf

--- driver.c
+++ driver.c
@@ -10,6 +10,10 @@ int sum5(int a, int b, int c, int d, int e) {
   return a + b + c + d + e;
 }
 
+void xprintf(char *fmt, char *arg) {
+ printf(fmt, arg);
+}
+
 int main(int argc, char **argv) {
   printf("%d\n", mymain());
   return 0;
--- test.sh
+++ test.sh
@@ -74,8 +74,8 @@ test 102 'a=1;b=48+2;c=a+b;c*2;'
 
 test 25 'sum2(20, 5);'
 test 15 'sum5(1, 2, 3, 4, 5);'
-test a3 'printf("a");3;'
-test abc5 'printf("%s", "abc");5;'
+test a3 'xprintf("a");3;'
+test abc5 'xprintf("%s", "abc");5;'
 
 testfail '0abc;'
 testfail '1+;'

6e624be15715d749881f4e340a2a31591ade3c64
DQNEO <dqneoo@gmail.com>
2018-04-30 23:05:54 +0900

    move some reader funcs

--- 8cc.go
+++ 8cc.go
@@ -146,36 +146,6 @@ func priority(op byte) int {
 	}
 }
 
-func read_number(n int) *Ast {
-	for {
-		ch := read_ch()
-		if !isdigit(ch.c) {
-			unget_ch(ch)
-			return make_ast_int(n)
-		}
-		n = n * 10 + int(ch.c - '0')
-	}
-}
-
-func read_ident(c byte) []byte {
-	buf := make([]byte, BUFLEN)
-	buf[0] = c
-	i := 1
-	for {
-		ch := read_ch()
-		if (!isalnum(ch.c)) {
-			unget_ch(ch)
-			break
-		}
-		buf[i] = ch.c
-		i++
-		if i == (BUFLEN -1) {
-			_error("Identifier too long")
-		}
-	}
-	buf[i] = 0;
-	return buf
-}
 
 func read_func_args(fname []byte) *Ast {
 	args := make([]*Ast, MAX_ARGS+1)
@@ -239,56 +209,6 @@ func read_prim() *Ast {
 	}
 	_error("Don't know how to handle '%c'", ch.c)
 	return nil
-}
-
-func read_char() *Ast {
-	ch := read_ch()
-	if ch == nil {
-		_error("Unterminated char")
-	}
-	if ch.c == '\\' {
-		ch = read_ch()
-		if ch == nil {
-			_error("Unterminated char")
-		}
-	}
-
-	ch2 := read_ch()
-	if ch2 == nil {
-		_error("Unterminated char")
-	}
-	if ch2.c != '\'' {
-		_error("Malformed char constant")
-	}
-
-	return make_ast_char(ch.c)
-}
-
-func read_string() *Ast {
-	buf := make([]byte, BUFLEN)
-	i := 0
-	for {
-		ch := read_ch()
-		if ch == nil {
-			_error("Unterminated string")
-		}
-		if ch.c == '"' {
-			break
-		}
-		if ch.c == '\\' {
-			ch = read_ch()
-			if ch == nil {
-				_error("Unterminated \\")
-			}
-		}
-		buf[i] = ch.c
-		i++
-		if i == BUFLEN - 1 {
-			_error("String too long")
-		}
-	}
-	buf[i] = 0
-	return make_ast_str(buf)
 }
 
 func read_expr2(prec int) *Ast {
--- lex.go
+++ lex.go
@@ -36,3 +36,85 @@ func is_punct(ch *char, c byte) bool {
 	}
 	return ch.c == c
 }
+
+func read_number(n int) *Ast {
+	for {
+		ch := read_ch()
+		if !isdigit(ch.c) {
+			unget_ch(ch)
+			return make_ast_int(n)
+		}
+		n = n * 10 + int(ch.c - '0')
+	}
+}
+
+func read_ident(c byte) []byte {
+	buf := make([]byte, BUFLEN)
+	buf[0] = c
+	i := 1
+	for {
+		ch := read_ch()
+		if (!isalnum(ch.c)) {
+			unget_ch(ch)
+			break
+		}
+		buf[i] = ch.c
+		i++
+		if i == (BUFLEN -1) {
+			_error("Identifier too long")
+		}
+	}
+	buf[i] = 0;
+	return buf
+}
+
+
+func read_char() *Ast {
+	ch := read_ch()
+	if ch == nil {
+		_error("Unterminated char")
+	}
+	if ch.c == '\\' {
+		ch = read_ch()
+		if ch == nil {
+			_error("Unterminated char")
+		}
+	}
+
+	ch2 := read_ch()
+	if ch2 == nil {
+		_error("Unterminated char")
+	}
+	if ch2.c != '\'' {
+		_error("Malformed char constant")
+	}
+
+	return make_ast_char(ch.c)
+}
+
+func read_string() *Ast {
+	buf := make([]byte, BUFLEN)
+	i := 0
+	for {
+		ch := read_ch()
+		if ch == nil {
+			_error("Unterminated string")
+		}
+		if ch.c == '"' {
+			break
+		}
+		if ch.c == '\\' {
+			ch = read_ch()
+			if ch == nil {
+				_error("Unterminated \\")
+			}
+		}
+		buf[i] = ch.c
+		i++
+		if i == BUFLEN - 1 {
+			_error("String too long")
+		}
+	}
+	buf[i] = 0
+	return make_ast_str(buf)
+}

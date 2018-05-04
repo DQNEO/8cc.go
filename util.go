5c8ac72c4798fd669dfe0b5c580dfabbe1145873
DQNEO <dqneoo@gmail.com>
2018-05-04 16:47:08 +0900

    Add warn

--- util.go
+++ util.go
@@ -9,3 +9,8 @@ func _error(format string, args ...interface{}) {
 	panic(fmt.Sprintf(format, args...))
 	os.Exit(1)
 }
+
+func warn(format string, args ...interface{}) {
+	fmt.Fprint(os.Stderr, "warning: ")
+	fmt.Fprintf(os.Stderr, format,args...)
+}

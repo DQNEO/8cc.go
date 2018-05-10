51a4390312e20abc8e41fbc59f022c986583cf71
DQNEO <dqneoo@gmail.com>
2018-05-11 02:24:08 +0900

    fix type of malloc

--- parse.c
+++ parse.c
@@ -444,7 +444,7 @@ Ast *read_decl_or_stmt(void) {
 }
 
 Ast **read_block(void) {
-  Ast **stmts = malloc(sizeof(Ast) * EXPR_LEN);
+  Ast **stmts = malloc(sizeof(Ast **) * EXPR_LEN);
   int i;
   for (i = 0; i < EXPR_LEN; i++) {
     Ast *t = read_decl_or_stmt();

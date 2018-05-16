cbba4bef44bd50054be80eaa4dce39a9324e7487
DQNEO <dqneoo@gmail.com>
2018-05-16 10:46:14 +0900

    separate ast constructor

--- main.c
+++ main.c
@@ -4,15 +4,18 @@
 
 #include <stdlib.h>
 
+Ast *ast_func(List *body) {
+  Ast *r = malloc(sizeof(Ast));
+  r->fname = "mymain";
+  r->locals = locals;
+  r->body = body;
+  return r;
+}
 int main(int argc, char **argv) {
   int wantast = (argc > 1 && !strcmp(argv[1], "-a"));
   List *block = read_block();
 
-  Ast *r = malloc(sizeof(Ast));
-  r->fname = "mymain";
-  r->locals = locals;
-  r->body = block;
-  
+  Ast *r = ast_func(block);
   if (wantast) {
     printf("%s", block_to_string(r->body));
   } else {

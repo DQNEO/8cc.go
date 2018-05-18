961ad8d6d3b1b1e2a1c3b276d26062777a8b89cc
DQNEO <dqneoo@gmail.com>
2018-05-18 10:29:37 +0900

    separate ast constructor

--- main.go
+++ main.go
@@ -4,15 +4,20 @@ import (
 	"os"
 )
 
+func ast_func(body []*Ast) *Ast {
+	r := &Ast{}
+	r.funcall.fname = []byte("mymain\x00")
+	r.funcall.locals = locals
+	r.funcall.body = body
+	return r
+}
+
 func main() {
 	initStdin()
 	wantast := len(os.Args) > 1 && os.Args[1] == "-a"
 	block := read_block()
 
-	r := &Ast{}
-	r.funcall.fname = []byte("mymain\x00")
-	r.funcall.locals = locals
-	r.funcall.body = block
+	r := ast_func(block)
 
 	if wantast {
 		printf("%s", block_to_string(r.funcall.body))

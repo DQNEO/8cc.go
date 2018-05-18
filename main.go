f53ac709a250c03fa985246e098b5eb06848e60b
DQNEO <dqneoo@gmail.com>
2018-05-18 10:33:41 +0900

    read_func_list

--- main.go
+++ main.go
@@ -12,18 +12,24 @@ func ast_func(body []*Ast) *Ast {
 	return r
 }
 
+func read_func_list() []*Ast {
+	block := read_block()
+	r := ast_func(block)
+	func_list := []*Ast{}
+	func_list = append(func_list, r)
+	return func_list
+}
+
 func main() {
 	initStdin()
 	wantast := len(os.Args) > 1 && os.Args[1] == "-a"
-	block := read_block()
-
-	r := ast_func(block)
-
+	func_list := read_func_list()
+	f := func_list[0]
 	if wantast {
-		printf("%s", block_to_string(r.funcall.body))
+		printf("%s", block_to_string(f.funcall.body))
 	} else {
 		emit_data_section()
-		emit_func(r)
+		emit_func(f)
 	}
 
 	return

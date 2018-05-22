cc1030bc03d687b9f3edf6909a2d1e912e171559
DQNEO <dqneoo@gmail.com>
2018-05-22 21:36:38 +0900

    tweak

--- lex.c
+++ lex.c
@@ -135,13 +135,13 @@ static Token *read_token_int(void) {
     case 'Q': case 'R': case 'S': case 'T': case 'U': case 'V': case 'W':
     case 'X': case 'Y': case 'Z': case '_':
       return read_ident(c);
+    case '=': {
+      return make_punct('=');
+    }
     case '/': case '*': case '+': case '-': case '(': case ')': case ',':
     case ';': case '&': case '[': case ']': case '{': case '}': case '<':
     case '>':
       return make_punct(c);
-    case '=': {
-      return make_punct('=');
-    }
     case EOF:
       return NULL;
     default:

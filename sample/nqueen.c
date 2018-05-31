f76b7f00f82a2b432ac9ef7722219cee586c32ee
DQNEO <dqneoo@gmail.com>
2018-05-31 23:33:40 +0900

    use single statement for if and for in nquee.c

--- sample/nqueen.c
+++ sample/nqueen.c
@@ -1,11 +1,10 @@
 int print_board(int board[][8]) {
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
-      if (board[i][j]) {
+      if (board[i][j])
         printf("Q ");
-      } else {
+      else
         printf(". ");
-      }
     }
     printf("\n");
   }
@@ -14,20 +13,15 @@ int print_board(int board[][8]) {
 
 int conflict(int board[][8], int row, int col) {
   for (int i = 0; i < row; i++) {
-    if (board[i][col]) {
+    if (board[i][col])
       return 1;
-    }
     int j = row - i;
-    if (0 < col - j + 1) {
-      if (board[i][col - j]) {
+    if (0 < col - j + 1)
+      if (board[i][col - j])
         return 1;
-      }
-    }
-    if (col + j < 8) {
-      if (board[i][col + j]) {
+    if (col + j < 8)
+      if (board[i][col + j])
         return 1;
-      }
-    }
   }
   return 0;
 }
@@ -49,8 +43,7 @@ int solve(int board[][8], int row) {
 int main() {
   int board[64];
   int i;
-  for (i = 0; i < 64; i++) {
+  for (i = 0; i < 64; i++)
     board[i] = 0;
-  }
   solve(board, 0);
 }

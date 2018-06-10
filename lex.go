8377f1804d201d6c97107d34047b6c0cb129f1e4
DQNEO <dqneoo@gmail.com>
2018-06-10 15:00:23 +0900

    Refactor:generalize

--- lex.go
+++ lex.go
@@ -145,13 +145,13 @@ func read_ident(c byte) *Token {
 	}
 }
 
-func read_rep() *Token {
+func read_rep(expect int, t1 int, t2 int) *Token {
 	c, _ := getc(stdin)
-	if c == '=' {
-		return make_punct(byte(PUNCT_EQ))
+	if c == byte(expect) {
+		return make_punct(byte(t2))
 	}
 	ungetc(c, stdin)
-	return make_punct('=')
+	return make_punct(byte(t1))
 }
 
 func read_token_init() *Token {
@@ -171,7 +171,7 @@ func read_token_init() *Token {
 	case ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_':
 		return read_ident(c)
 	case c == '=':
-		return read_rep()
+		return read_rep(int('='), int('='), PUNCT_EQ)
 	case c == '/' || c == '*' ||
 		c == '+' || c == '-' || c == '(' ||
 		c == ')' || c == ',' || c == ';' || c == '&' ||

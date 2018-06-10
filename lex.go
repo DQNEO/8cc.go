2c65ceb54826e005cd03dd41268543766cb0d019
DQNEO <dqneoo@gmail.com>
2018-06-10 14:28:45 +0900

    extract func

--- lex.go
+++ lex.go
@@ -145,6 +145,15 @@ func read_ident(c byte) *Token {
 	}
 }
 
+func read_rep() *Token {
+	c, _ := getc(stdin)
+	if c == '=' {
+		return make_punct(byte(PUNCT_EQ))
+	}
+	ungetc(c, stdin)
+	return make_punct('=')
+}
+
 func read_token_init() *Token {
 	c, err := getc_nonspace()
 	if err != nil {
@@ -162,12 +171,7 @@ func read_token_init() *Token {
 	case ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_':
 		return read_ident(c)
 	case c == '=':
-		c, _ = getc(stdin)
-		if c == '=' {
-			return make_punct(byte(PUNCT_EQ))
-		}
-		ungetc(c, stdin)
-		return make_punct('=')
+		return read_rep()
 	case c == '/' || c == '*' ||
 		c == '+' || c == '-' || c == '(' ||
 		c == ')' || c == ',' || c == ';' || c == '&' ||

c0b287eb4e65041eb73ab23df62aea4f8e7e85e9
DQNEO <dqneoo@gmail.com>
2018-06-15 22:43:52 +0900

    move

--- lex.go
+++ lex.go
@@ -166,6 +166,12 @@ func read_token_init() *Token {
 		return read_number(c)
 	case ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_':
 		return read_ident(c)
+	case c == '/' || c == '*' || c == '(' ||
+		c == ')' || c == ',' || c == ';' ||
+		c == '[' || c == ']' || c == '{' || c == '}' ||
+		c == '<' || c == '>' || c == '!' ||
+		c == '?' || c == ':':
+		return make_punct(c)
 	case c == '=': return read_rep(int('='), int('='), PUNCT_EQ)
 	case c == '+': return read_rep(int('+'), int('+'), PUNCT_INC)
 	case c == '-': return read_rep(int('-'), int('-'), PUNCT_DEC)
@@ -173,12 +179,6 @@ func read_token_init() *Token {
 	case c == '|': return read_rep(int('|'), int('|'), PUNCT_LOGOR)
 	case c == '"': return read_string()
 	case c == '\'': return read_char()
-	case c == '/' || c == '*' || c == '(' ||
-		c == ')' || c == ',' || c == ';' ||
-		c == '[' || c == ']' || c == '{' || c == '}' ||
-		c == '<' || c == '>' || c == '!' ||
-		c == '?' || c == ':':
-		return make_punct(c)
 	default:
 		_error("Don't know how to handle '%c'", c)
 	}

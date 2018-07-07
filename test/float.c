b295746a8995e9cee8e56f0baf068b0c99880df9
DQNEO <dqneoo@gmail.com>
2018-07-07 20:47:07 +0900

    rename func

--- test/float.c
+++ test/float.c
@@ -1,4 +1,4 @@
-int expect(float a, float b) {
+int expectf(float a, float b) {
     if (!(a == b)) {
         printf("Failed\n");
         printf("  %f expected, but got %f\n", a, b);
@@ -9,16 +9,16 @@ int expect(float a, float b) {
 int main() {
     printf("Testing float ... ");
 
-    expect(1.0, 1.0);
-    expect(1.5, 1.0 + 0.5);
-    expect(0.5, 1.0 - 0.5);
-    expect(2.0, 1.0 * 2.0);
-    expect(0.25, 1.0 / 4.0);
+    expectf(1.0, 1.0);
+    expectf(1.5, 1.0 + 0.5);
+    expectf(0.5, 1.0 - 0.5);
+    expectf(2.0, 1.0 * 2.0);
+    expectf(0.25, 1.0 / 4.0);
 
-    expect(3.0, 1.0 + 2);
-    expect(2.5, 5 - 2.5);
-    expect(2.0, 1.0 * 2);
-    expect(0.25, 1.0 / 4);
+    expectf(3.0, 1.0 + 2);
+    expectf(2.5, 5 - 2.5);
+    expectf(2.0, 1.0 * 2);
+    expectf(0.25, 1.0 / 4);
 
     printf("OK\n");
     return 0;

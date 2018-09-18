71f4a53b1716daf663fc2442b1de38256be1ca2f
DQNEO <dqneoo@gmail.com>
2018-09-18 20:01:37 +0900

    Rename local var

--- cpp.c
+++ cpp.c
@@ -532,8 +532,8 @@ static void read_include(void) {
     expect_newline();
 
     List *paths = std ? std_include_path : make_list1("");
-    FILE *file = open_header_file(name, paths);
-    push_input_file(file);
+    FILE *fp = open_header_file(name, paths);
+    push_input_file(fp);
 }
 
 static void read_directive(void) {

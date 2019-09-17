935229018bd91a788a2447a3c2011b9c6d7aae07
DQNEO <dqneoo@gmail.com>
2019-09-17 10:51:25 +0900

    Change order

--- parse.c
+++ parse.c
@@ -944,13 +944,14 @@ static void read_decl_spec(Ctype **rtype, int *sclass) {
     }
     switch (ti) {
     case kchar: *rtype = (sig != kunsigned) ? ctype_char : ctype_uchar; return;
-    case kshort: *rtype =  (sig != kunsigned) ? ctype_short : ctype_ushort; return;
-    case kint: *rtype = (sig != kunsigned) ? ctype_int: ctype_uint; return;
-    case klong:
-    case kllong: *rtype =  (sig != kunsigned) ? ctype_long : ctype_ulong; return;
     case kfloat: *rtype = ctype_float; return;
     case kdouble: *rtype = ctype_double; return;
+
+    case kshort: *rtype =  (sig != kunsigned) ? ctype_short : ctype_ushort; return;
+    case klong:
+    case kllong: *rtype =  (sig != kunsigned) ? ctype_long : ctype_ulong; return;
     case kvoid: *rtype = ctype_void; return;
+    case kint: *rtype = (sig != kunsigned) ? ctype_int: ctype_uint; return;
     }
     error("internal error");
  err:

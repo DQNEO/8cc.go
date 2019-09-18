02e59697175e2bae7a8aebda2936bcabcab92baf
DQNEO <dqneoo@gmail.com>
2019-09-18 22:09:28 +0900

    port 11542eba68674c0c6ba1f132c5474b3e1acdd43c

--- parse.go
+++ parse.go
@@ -806,7 +806,7 @@ func is_type_keyword(tok *Token) bool {
 
 	keyword := []string{
 		"char", "short", "int", "long", "float", "double", "struct",
-		"union", "signed", "unsigned", "enum", "void", "extern",
+		"union", "signed", "unsigned", "enum", "void", "extern", "typedef",
 	}
 	for _, k := range keyword {
 		if k == tok.sval {
